bdc56f3f102e74021737b00b1b594d1e7b0a20ec
Satria Janaka <satria.janaka@hotmail.com>
2020-01-13 16:33:35 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Belajar react
         </a>
       </header>
     </div>
